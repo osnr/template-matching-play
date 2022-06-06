4a480e2851ede248112470f2a84aeb160e5e9ddc
Omar Rizwan <omar@omar.website>
2022-06-05 22:34:32 -0400

    fix myfftconvolve!

--- myfftconvolve.py
+++ myfftconvolve.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 
-def myfftconvolve(f, g, mode):  # our impl
+# from https://dsp.stackexchange.com/questions/63147/how-to-manually-implement-convolution-with-ffts
+def myfftconvolve(f, g, mode):
     # Pad g to equal size of f. This assumes g is smaller in both dimensions
     # and that the difference between f and g dimensions are even
     p1, p2 = (np.r_[f.shape]-g.shape)/2
 
     gpad = np.pad(g, ((np.int(np.floor(p1)), np.int(np.ceil(p1))),
                       (np.int(np.floor(p2)), np.int(np.ceil(p2)))),
-                  mode='edge')
+                  mode='constant')
 
     # Shift g to 'center' on top left corner (the 'origin' in an fft)
     gpad = np.fft.ifftshift(gpad)
